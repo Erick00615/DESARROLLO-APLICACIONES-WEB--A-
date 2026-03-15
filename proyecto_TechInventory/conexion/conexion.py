68672f0de3c096123c12db9372cf29607af580fa
Erick Sarchi <ea.sarchig@uea.edu.ec>
2026-03-14 19:42:44 -0500

    Actualizar conexion.py: puerto 3307 local, listo para Render con variables

--- proyecto_TechInventory/conexion/conexion.py
+++ proyecto_TechInventory/conexion/conexion.py
@@ -3,11 +3,11 @@ import os
 
 def obtener_conexion():
     conexion = mysql.connector.connect(
-        host=os.environ.get("MYSQL_HOST"),
-        user=os.environ.get("MYSQL_USER"),
-        password=os.environ.get("MYSQL_PASSWORD"),
-        database=os.environ.get("MYSQL_DB"),
-        port=int(os.environ.get("MYSQL_PORT", 3306))  # 3306 por defecto
+        host=os.environ.get("MYSQL_HOST", "127.0.0.1"),
+        user=os.environ.get("MYSQL_USER", "root"),
+        password=os.environ.get("MYSQL_PASSWORD", "123456"),
+        database=os.environ.get("MYSQL_DB", "techinventory"),
+        port=int(os.environ.get("MYSQL_PORT", 3307))  # <-- 3307 para local
     )
     return conexion
 
